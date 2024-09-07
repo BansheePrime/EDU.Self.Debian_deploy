0f6958b8f4e81ca5a909cd9e4895ae2222987925
BansheePrime <cmapuk-xpehob@yandex.ru>
2024-09-08 01:02:39 +0300

    first steps in powershell vm creation

---
+++ scripts/create_debian_vm.ps1
@@ -0,0 +1,8 @@
+## https://learn.microsoft.com/en-us/powershell/module/hyper-v/new-vm?view=windowsserver2022-ps
+## $PSVersionTable
+
+## New-VM
+New-VM -Name "???" -Generation 2 -BootDevice CD -NoVHD
+
+## Set-VM
+Set-VM -Name "???" -AutomaticStopAction Shutdown -ProcessorCount 2
